54bfa9a93fc79daf59c38f631ab659bdbb389896
Abdel El-Medny <abdel@springtree.nl>
2018-03-14 23:05:02 +0100

    feat(landing): adds landing page

--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -2,11 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
+import { LandingComponent } from './components/landing/landing.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
