ec66e586393ff2ffd95273b424ef51618fba195a
Abdel El-Medny <abdel@springtree.nl>
2018-03-25 22:59:03 +0200

    feat(product-list): adds initial version of product list

--- packages/client/src/app/modules/webshop/webshop.module.ts
+++ packages/client/src/app/modules/webshop/webshop.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { Routes, RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+
+
+const routes: Routes = [
+  { path: '', component: ProductListComponent }
+];
+
+export const RoutingConfig: ModuleWithProviders = RouterModule.forChild(routes);
+
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatSelectModule,
+    RoutingConfig
   ],
   declarations: [ProductListComponent, ProductDetailsComponent]
 })
