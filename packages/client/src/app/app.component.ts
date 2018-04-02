aa697130417e9e16d9562496b9fee843801ba863
Abdel El-Medny <abdel@springtree.nl>
2018-04-02 17:24:30 +0200

    refactor(app): adds html file for entry file

--- packages/client/src/app/app.component.ts
+++ packages/client/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { routerTransition } from './animations';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <fys-header></fys-header>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: './app.component.html',
+  animations: [routerTransition]
 })
-export class AppComponent { }
+export class AppComponent {
+  getState(outlet: RouterOutlet) {
+     const state = outlet.activatedRouteData.state;
+     console.log('[app-root] router state is', state);
+
+     return state;
+  }
+}
