8efa925ebcb1d7d0a71986bd6aab11fb6f968bc2
Abdel El-Medny <abdel@springtree.nl>
2018-04-02 20:42:03 +0200

    refactor(landing): makes content a bit more dynamic

--- packages/client/src/app/components/landing/landing.component.ts
+++ packages/client/src/app/components/landing/landing.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface ICategoryTile {
+  title: string;
+  icon: string;
+  description: string;
+  route?: string;
+}
+
 @Component({
   selector: 'fys-landing',
   templateUrl: './landing.component.html',
@@ -7,8 +14,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
+  public categories: ICategoryTile[] = [
+    { title: 'Taxfree Shop', icon: 'shop', description: this.loremText(), route: 'webshop'},
+    { title: 'Gaming', icon: 'videogame_asset', description: this.loremText(), route: 'tic-tac-toe'},
+    { title: 'Flight info', icon: 'my_location', description: this.loremText()},
+    { title: 'Video', icon: 'video_library', description: `Entertain yourself with a monthly selection of videos`},
+    { title: 'Audio', icon: 'audiotrack', description: this.loremText()}
+  ];
+
   constructor() { }
 
   ngOnInit() { }
+
+  public loremText() {
+    return `Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi repellendus placeat, fugit voluptate molestias distinctio
+      at tenetur commodi veritatis tempora dolor beatae nostrum quasi, blanditiis non. Suscipit reprehenderit voluptas qui.`;
+  }
 
 }
