2d17c0fb7a983d0f983fdea3155a759c8f4f41ce
Abdel El-Medny <abdel@springtree.nl>
2018-04-19 21:21:03 +0200

    style(app): fixes linting errors

--- packages/client/src/app/app.component.ts
+++ packages/client/src/app/app.component.ts
@@ -3,15 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { routerTransition } from './animations';
 
 @Component({
-  selector: 'app-root',
+  selector: 'fys-root',
   templateUrl: './app.component.html',
   animations: [routerTransition]
 })
 export class AppComponent {
   getState(outlet: RouterOutlet) {
      const state = outlet.activatedRouteData.state;
-     console.log('[app-root] router state is', state);
-
      return state;
   }
 }
