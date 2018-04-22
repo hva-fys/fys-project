c6515cc283c9f7358756ca92ac7f7b882fa7b31f
Abdel El-Medny <abdel@springtree.nl>
2018-04-22 23:02:45 +0200

    feat(animations): speeds up animation by 100ms

--- packages/client/src/app/animations.ts
+++ packages/client/src/app/animations.ts
@@ -40,7 +40,7 @@ export const routerTransition = trigger('routerTransition', [
       query(':enter', style({ opacity: 1 }), { optional: true }),
       query(':leave', style({ opacity: 0 }), { optional: true }),
     ]),
-    query(':enter mat-card', stagger(200, [
+    query(':enter mat-card', stagger(100, [
       style({ transform: 'translateY(100px)' }),
       animate('150ms ease-in-out',
         style({ transform: 'translateY(0px)', opacity: 1 })),
