a133d2425d0e52d7090f4687f456a90ce2f921be
Abdel El-Medny <abdel@springtree.nl>
2018-04-22 23:01:29 +0200

    feat(checkout): finishes initial version

--- packages/client/src/app/modules/webshop/checkout/checkout.component.ts
+++ packages/client/src/app/modules/webshop/checkout/checkout.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { filter, map } from 'rxjs/operators';
+import { StateService, ICartLine } from '../state.service';
+import { IProduct } from '../products';
 
 @Component({
   selector: 'fys-checkout',
@@ -7,7 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckoutComponent implements OnInit {
 
-  constructor() { }
+  public displayedColumns = ['imageUrl', 'name', 'price', 'quantity', 'total'];
+
+  public total$: Observable<number> = this.state$.cart$.pipe(
+    filter(cart => Boolean(cart)),
+    map(cart => cart.total)
+  );
+
+  public products$: Observable<IProduct[]> = this.state$.cart$.pipe(
+    map( cart => cart.lines),
+    map( lines => {
+
+      const sum = (a, b) => a + b;
+
+      const quantity = lines.map( line => line.quantity ).reduce(sum, 0);
+
+      const total = lines.map( line => line.quantity * line.price ).reduce(sum, 0);
+
+
+      const newLine: ICartLine = {
+        id: null,
+        imageUrl: null,
+        name: 'Total',
+        price: total,
+        quantity,
+      };
+
+      const newLines = [...lines, newLine];
+
+      return newLines;
+    })
+  );
+
+  constructor(private state$: StateService) { }
 
   ngOnInit() {
   }
