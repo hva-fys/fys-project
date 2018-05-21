b2d16bf5f2407b0eb293b2e6215cd6eed2294f5c
Abdel El-Medny <abdel@springtree.nl>
2018-05-21 20:55:14 +0200

    feat(flight-status): work in progress UI

--- packages/client/src/app/modules/flight-status/flight-status/flight-status.component.ts
+++ packages/client/src/app/modules/flight-status/flight-status/flight-status.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { style, animate, trigger, transition, query, stagger, keyframes } from '@angular/animations';
+import { FlightStatusService } from '../../../services/flight-status.service';
+import { FlightInformation } from 'fys';
+import { WikipediaService } from '../../../services/wikipedia.service';
+
 @Component({
   selector: 'fys-flight-status',
   templateUrl: './flight-status.component.html',
@@ -27,9 +31,23 @@ export class FlightStatusComponent implements OnInit {
 
   num = 0;
 
+  public images = {
+    cc: ['./assets/images/locations/cc/1.jpg', './assets/images/locations/cc/2.jpg', './assets/images/locations/cc/3.jpg'],
+    hh: ['./assets/images/locations/hh/1.jpg', './assets/images/locations/hh/2.jpg', './assets/images/locations/hh/3.jpg']
+  };
+
+  public airplanes: Map<FlightInformation.TAirplane, string> = new Map();
+
   speedLabelFn: Function = () => `${this.num} km/h`;
 
-  constructor() { }
+  constructor(private $flightStatus: FlightStatusService, private $wikipedia: WikipediaService) {
+    this.airplanes.set('a320', './assets/images/airplanes/a320.jpg');
+    this.airplanes.set('737300', './assets/images/airplanes/737300.jpg');
+    this.airplanes.set('737400', './assets/images/airplanes/737400.jpg');
+    this.airplanes.set('737800', './assets/images/airplanes/737800.jpg');
+
+    this.$wikipedia.getIntro('Cape Canaveral').subscribe();
+  }
 
   ngOnInit() {
 
@@ -44,6 +62,9 @@ export class FlightStatusComponent implements OnInit {
       lat: this.curr.lat + latSteps,
       lng: this.curr.lng + lngSteps
     };
+
+    this.lat = this.curr.lat + latSteps;
+    this.lng = this.curr.lng + lngSteps;
   }
 
 }
