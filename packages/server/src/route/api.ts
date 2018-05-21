91ba345395eec030d21bf53005aa0656e4893b4e
Abdel El-Medny <abdel@springtree.nl>
2018-05-21 16:02:52 +0200

    feat(api): adds flight info route

--- packages/server/src/route/api.ts
+++ packages/server/src/route/api.ts
@@ -1,5 +1,38 @@
 import * as express from 'express';
 import * as env from "../environment";
+import { sample } from 'lodash';
+import { FlightInformation } from '../../../shared/fys-types';
+
+const flightInformation: FlightInformation.IFlight[] = [
+    {
+        start: {
+            latitude: "34.052234",
+            longitude: "-118.243685",
+            name: "Los Angeles",
+            shortHand: "LA"
+        },
+        end: {
+            latitude: "21.3069444",
+            longitude: "-157.8583333",
+            name: "Honolulu",
+            shortHand: "HI"
+        }
+    },
+    {
+        start: {
+            latitude: "40.730610",
+            longitude: "-73.935242",
+            name: "New york",
+            shortHand: "NY"
+        },
+        end: {
+            latitude: "28.396837",
+            longitude: "-80.605659",
+            name: "Cape Canaveral",
+            shortHand: "CC"
+        }
+    }
+];
 
 /**
  * Auth route segment.
@@ -15,6 +48,12 @@ module.exports = (Environment: env.Environment) => {
         return res.json({
             name: 'The lion king',
         });
+    });
+
+    router.get('/flightInfo', (req, res) => {
+        const randomFlight = sample(flightInformation);
+
+        res.json(randomFlight);
     });
 
     return router;
