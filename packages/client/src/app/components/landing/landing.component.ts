75223d9719e2f5f3deea70ae7c3d2eebe8102b05
Abdel El-Medny <abdel@springtree.nl>
2018-05-22 11:56:46 +0200

    feat(landing): adds better descriptions of app routes

--- packages/client/src/app/components/landing/landing.component.ts
+++ packages/client/src/app/components/landing/landing.component.ts
@@ -15,11 +15,23 @@ interface ICategoryTile {
 export class LandingComponent implements OnInit {
 
   public categories: ICategoryTile[] = [
-    { title: 'Taxfree Shop', icon: 'shop', description: this.loremText(), route: 'webshop'},
-    { title: 'Gaming', icon: 'videogame_asset', description: this.loremText(), route: 'tic-tac-toe'},
-    { title: 'Flight info', icon: 'my_location', description: this.loremText(), route: 'flight-status'},
-    { title: 'Video', icon: 'video_library', description: `Entertain yourself with a monthly selection of videos`},
-    { title: 'Audio', icon: 'audiotrack', description: this.loremText(), route: 'audio' }
+    {
+      title: 'Taxfree Shop', icon: 'shop',
+      description: `Do you want to take advantage of your trip as much as possible? View our tax-free products here!`,
+      route: 'webshop'
+    },
+    { title: 'Gaming', icon: 'videogame_asset',
+      description: `Are you just staring at the clouds and do you want some variety? Test your gaming skills with one of our games.`,
+      route: 'tic-tac-toe'
+    },
+    { title: 'Flight info', icon: 'my_location',
+      description: `Hold on tight, and enjoy the Flight! Track your flight with live information from the cockpit.`,
+      route: 'flight-status'},
+    { title: 'Video', icon: 'video_library',
+      description: `Do you just settle down and laugh at a comedy? Or prefer to let time fly with watching an exciting thriller?`},
+    { title: 'Audio', icon: 'audiotrack',
+      description: `Listen to music from our wide range and dream away while we take you to your destination.`,
+      route: 'audio' }
   ];
 
   constructor() { }
