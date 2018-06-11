2e4ea34b0c559ec6021edec77219a8d55ab9cae1
Xenooo <milogisolf@hotmail.com>
2018-06-11 21:26:53 +0200

    feat(tracks): adds more tracks

--- packages/client/src/app/modules/audio/tracks.ts
+++ packages/client/src/app/modules/audio/tracks.ts
@@ -11,5 +11,41 @@ export const tracks: ITracks[] = [
   {
     name: 'Milo track',
     trackSrc: './assets/tracks/never_gunna_give_you_up.mp3',
-  }
+  },
+  {
+    name: 'Frontliner - Spacer',
+    trackSrc: './assets/tracks/frontliner-spacer-prophets-hardcore-remuxx.mp3',
+  },
+  {
+    name: 'Skylord - Summerfire',
+    trackSrc: './assets/tracks/Skylord_Summerfire.mp3',
+  },
+  {
+    name: 'Skylord - Triumphant Victory',
+    trackSrc: './assets/tracks/Skylord - Triumphant Victory.mp3',
+  },
+  {
+    name: 'Vesania - Neurodeliri',
+    trackSrc: './assets/tracks/vesania-neurodeliri.mp3',
+  },
+  {
+    name: 'Mushroom Head - Crazy',
+    trackSrc: './assets/tracks/mushroom-head-crazy.mp3',
+  },
+  {
+    name: 'Mushroomhead - 12 Hunderd',
+    trackSrc: './assets/tracks/mushroomhead-12-hundred.mp3',
+  },
+  {
+    name: 'Mushroomhead - Almost Gone',
+    trackSrc: './assets/tracks/mushroomhead-almost-gone.mp3',
+  },
+  {
+    name: 'Mushroomhead - Becoming Cold',
+    trackSrc: './assets/tracks/mushroomhead-becoming-cold.mp3',
+  },
+  {
+    name: 'Mushroomhead - Before I Die',
+    trackSrc: './assets/tracks/mushroomhead-before-i-die.mp3',
+  },
 ];
