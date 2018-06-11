1116adabde24776c1297b431ff4cda3c4d08afaa
Abdel El-Medny <abdel@springtree.nl>
2018-06-11 20:13:50 +0200

    feat(tracks): removes id

--- packages/client/src/app/modules/audio/tracks.ts
+++ packages/client/src/app/modules/audio/tracks.ts
@@ -1,18 +1,15 @@
 export interface ITracks {
-  id: number;
   name: string;
   trackSrc: string;
 }
 
 export const tracks: ITracks[] = [
   {
-    id: 1,
     name: 'Never gonna give you up',
     trackSrc: './assets/tracks/never_gunna_give_you_up.mp3',
   },
   {
-    id: 2,
-    name: 'Track 2',
-    trackSrc: './assets/track/',
+    name: 'Milo track',
+    trackSrc: './assets/tracks/never_gunna_give_you_up.mp3',
   }
 ];
