abf3613a4c7b66c6612707c6746247c87ea8f47e
Abdel El-Medny <abdel@springtree.nl>
2018-06-17 21:45:16 +0200

    feat(gaming-home): adds game descriptions

--- packages/client/src/app/modules/gaming/gaming-home/gaming-home.component.ts
+++ packages/client/src/app/modules/gaming/gaming-home/gaming-home.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { WikipediaService } from '../../../services/wikipedia.service';
+import { fadeInOut } from '../../../animations';
 
 interface IGameCard {
   route: string;
   name: string;
+  description: Observable<string>;
 }
 
 @Component({
   selector: 'fys-gaming-home',
   templateUrl: './gaming-home.component.html',
-  styleUrls: ['./gaming-home.component.scss']
+  styleUrls: ['./gaming-home.component.scss'],
+  animations: [fadeInOut]
 })
 export class GamingHomeComponent implements OnInit {
 
@@ -16,19 +21,22 @@ export class GamingHomeComponent implements OnInit {
   public games: IGameCard[] = [
     {
       name: 'Tic tac toe',
-      route: '../tic-tac-toe'
+      route: '../tic-tac-toe',
+      description: this.$wikipedia.getIntro('Tic-tac-toe')
     },
     {
       name: 'Snake',
-      route: 'snake'
+      route: 'snake',
+      description: this.$wikipedia.getIntro('Snake (video game genre)')
     },
     {
       name: 'Pong',
-      route: 'pong'
+      route: 'pong',
+      description: this.$wikipedia.getIntro('pong')
     }
   ];
 
-  constructor() { }
+  constructor(private $wikipedia: WikipediaService) { }
 
   ngOnInit() {
   }
