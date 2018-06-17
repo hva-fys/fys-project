e975d10d4e6cb709f2df9145b2c3885d2e2370b3
Abdel El-Medny <abdel@springtree.nl>
2018-06-17 20:49:21 +0200

    feat(routes): Adds gaming route

--- packages/client/src/app/app.routing.ts
+++ packages/client/src/app/app.routing.ts
@@ -5,12 +5,13 @@ import { LandingComponent } from './components/landing/landing.component';
 
 export const routes: Routes = [
   { path: '', component: LandingComponent },
-  { path: 'tic-tac-toe',    loadChildren: 'app/modules/tic-tac-toe/tic-tac-toe.module#TicTacToeModule' },
   { path: 'webshop',        loadChildren: 'app/modules/webshop/webshop.module#WebshopModule' },
   { path: 'audio',          loadChildren: 'app/modules/audio/audio.module#AudioModule' },
   { path: 'video',          loadChildren: 'app/modules/video/video.module#VideoModule' },
   { path: 'login',          loadChildren: 'app/modules/login/login.module#LoginModule' },
   { path: 'flight-status',  loadChildren: 'app/modules/flight-status/flight-status.module#FlightStatusModule' },
+  { path: 'gaming',         loadChildren: 'app/modules/gaming/gaming.module#GamingModule' },
+  { path: 'tic-tac-toe',    loadChildren: 'app/modules/tic-tac-toe/tic-tac-toe.module#TicTacToeModule' }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
